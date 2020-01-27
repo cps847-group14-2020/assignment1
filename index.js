ee437029f44845122abbb05a7e4a33e2cc26912e
Leigh Silverstein <l2silver@ryerson.ca>
2020-01-27 13:45:20 -0500

    bot echoing back

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const SlackBot = require('slackbots');
 const fetch = require('isomorphic-fetch');
+const express = require('express')
+const app = express()
+const port = 3000
+const { WebClient } = require('@slack/web-api');
+const token = process.env.SLACK_OAUTH_TOKEN;
+const web = new WebClient(token);
 
+app.use(express.json());
+app.get('/', (req, res) => {
+  console.log('YES SOMETHING');
+  res.send('Hello World!')
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.set('Content-type', 'application/json');
+  if(req.body.challenge)
+    return res.send({
+      challenge: req.body.challenge
+    });
+  switch(req.body.event.type){
+    case "app_mention":
+      web.chat.postMessage({
+        text: req.body.event.text.replace(/\<.*\>/, ""),
+        channel: req.body.event.channel,
+      });
+      return res.send(null);
+      break;
+    default:
+      return res.send(null);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
